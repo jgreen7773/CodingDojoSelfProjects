0087305cc0b9351c9c1e657ea6f512879ba33d78
Jamie Green (Song Kran) <James@DeathRiddle-iMac.local>
2019-08-19 12:59:10 -0700

    SS1_CodingDojo_SelfProjects

---
+++ answers.js
@@ -0,0 +1,44 @@
+myNumber = 42
+myName = 'Jamie'
+function printing() {
+for (i=-52; i<1067; i++)
+console.log(i);
+return (a);
+}
+
+function beCheerful() {
+    var str = 'good morning!'
+    for(cheers=1; cheers<99; chears++)
+    console.log(cheers)
+}
+
+function multiples_of_3() {
+    for (three = -300; three == 0; three = num+3){
+        if(three != -6 || -3){
+            console.log(three)
+        }
+    }
+}
+
+var x = 2000
+while(x<=5280;x++){
+    console.log(x)
+    if(x==5280){
+        break;
+    }
+}
+
+function birthday(){
+    prompt("What is my birthday?");
+    if(birthday == 14){
+        console.log("How did you know?")
+    }
+    else{
+        console.log("Just another day....")
+    }
+}
+
+function leapyear(){
+    for(leapyear=1991; leapyear<=2991; leapyear++)
+    
+}
